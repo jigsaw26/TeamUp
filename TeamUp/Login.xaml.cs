19042a2b4bef1e205baa429f247ea0108e3676ee
jigsaw <jigsaw@DESKTOP-BP347RE>
2022-10-25 10:17:50 +0300

    Добавьте файлы проекта.

---
+++ TeamUp/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamUp
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Register reg = new Register();
+            reg.Show();
+            Close();
+        }
+    }
+}
---
+++ TeamUp/Settings.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamUp
+{
+    /// <summary>
+    /// Логика взаимодействия для Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            btn1.Visibility = Visibility.Visible;
+            btn2.Visibility = Visibility.Hidden;
+            btn3.Visibility = Visibility.Hidden;
+            btn4.Visibility = Visibility.Hidden;
+            this.IsEnabled = true;
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            btn1.Visibility = Visibility.Hidden;
+            btn2.Visibility = Visibility.Visible;
+            btn3.Visibility = Visibility.Hidden;
+            btn4.Visibility = Visibility.Hidden;
+            this.IsEnabled = true;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            btn1.Visibility = Visibility.Hidden;
+            btn2.Visibility = Visibility.Hidden;
+            btn3.Visibility = Visibility.Visible;
+            btn4.Visibility = Visibility.Hidden;
+            this.IsEnabled = true;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            btn1.Visibility = Visibility.Hidden;
+            btn2.Visibility = Visibility.Hidden;
+            btn3.Visibility = Visibility.Hidden;
+            btn4.Visibility = Visibility.Visible;
+            this.IsEnabled = true;
+        }
+    }
+}
