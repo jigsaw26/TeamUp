50fd72638544dccfc8fcf0d3fa594fafc1a6ebbf
Pavel Lupashko <pavel.lupashko228@gmail.com>
2022-11-08 02:19:35 +0200

    Логин и пароль
    Добавил регистрацию и ввод логина с паролем

--- TeamUp/Login.xaml.cs
+++ TeamUp/Login.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace TeamUp
 {
@@ -26,8 +27,7 @@ namespace TeamUp
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) //registration click
-        {
-
+        { 
             Register reg = new Register();
             reg.Show();
             Close();
@@ -35,9 +35,50 @@ namespace TeamUp
 
         private void Button_Click_1(object sender, RoutedEventArgs e) //profile click
         {
-            Profile prof = new Profile();
-            prof.Show();
-            Close();
+            if (Text_Email.Text == "" || Text_Password.Password == "")
+                MessageBox.Show("Заполните все поля");
+            else
+            {
+                int Check = CheckEmail();
+                if (Check == 0) MessageBox.Show("Не верно введен логин и пароль");
+                else if (Check == 1) MessageBox.Show("Не верно введен логин или пароль");
+                else 
+                { 
+                    Profile profile = new Profile();
+                    profile.Show();
+                    Close();
+                } 
+            } 
+        }
+
+        public int CheckEmail()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+             
+            int y = 0;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("email");
+                    if (attr != null) if (attr.Value == Text_Email.Text) y++;
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        if (y == 1)
+                        {
+                            if (xnode.Attributes.Count > 0)
+                            {
+                                XmlNode attr1 = childnode.Attributes.GetNamedItem("pas");
+                                if (attr != null) if (attr.Value == Text_Password.Password) y++;
+                            } 
+                        }
+                        if (y == 2) break;
+                    }
+                }  
+            }
+            return y;
         }
     }
 }
--- TeamUp/Register.xaml.cs
+++ TeamUp/Register.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace TeamUp
 {
@@ -26,10 +27,67 @@ namespace TeamUp
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Profile profile = new Profile();
-            profile.Show();
-            Close();
+        {  
+            if (Text_Name.Text == "" || Text_Surname.Text == "" || Text_Counrty.Text == "" || Text_Date.Text == "" || Text_Email.Text == "" || Text_Password.Password == "" || Text_Password2.Password == "")
+                MessageBox.Show("Заполните все поля");
+            else if (Text_Password.Password != Text_Password2.Password)
+                MessageBox.Show("Пароль не совпадает"); 
+            else
+            {
+                int Check = CheckEmail();
+                if (Check == 1) MessageBox.Show("Такой пользователь уже существует");
+                else
+                { 
+                    string[] AtribteName = new string[6] { "User", "Name", "Surname", "Country", "Date", "Password" }; 
+                    XmlDocument xDoc = new XmlDocument();
+                    xDoc.Load("Users.xml"); 
+                    XmlElement xRoot = xDoc.DocumentElement;
+
+                    XmlElement KatalElem = xDoc.CreateElement(AtribteName[0]);
+                    xRoot.AppendChild(KatalElem);
+                    XmlAttribute nameAttr = xDoc.CreateAttribute("email");
+                    nameAttr.Value = Text_Email.Text;
+                    KatalElem.Attributes.Append(nameAttr);
+
+                    XmlElement AtrElem;
+                    XmlText Text;
+
+                    AtrElem = xDoc.CreateElement(AtribteName[1]);
+                    KatalElem.AppendChild(AtrElem);
+                    Text = xDoc.CreateTextNode(Text_Name.Text);
+                    AtrElem.AppendChild(Text);
+
+
+                    AtrElem = xDoc.CreateElement(AtribteName[2]);
+                    KatalElem.AppendChild(AtrElem);
+                    Text = xDoc.CreateTextNode(Text_Surname.Text);
+                    AtrElem.AppendChild(Text);
+
+
+                    AtrElem = xDoc.CreateElement(AtribteName[3]);
+                    KatalElem.AppendChild(AtrElem);
+                    Text = xDoc.CreateTextNode(Text_Counrty.Text);
+                    AtrElem.AppendChild(Text);
+
+
+                    AtrElem = xDoc.CreateElement(AtribteName[4]);
+                    KatalElem.AppendChild(AtrElem);
+                    Text = xDoc.CreateTextNode(Text_Date.Text);
+                    AtrElem.AppendChild(Text);
+
+
+                    AtrElem = xDoc.CreateElement(AtribteName[5]);
+                    KatalElem.AppendChild(AtrElem);
+                    Text = xDoc.CreateTextNode(Text_Password.Password);
+                    AtrElem.AppendChild(Text);
+                     
+                    xDoc.Save("Users.xml");
+
+                    Profile profile = new Profile();
+                    profile.Show();
+                    Close();
+                }
+            } 
         }
 
         private void Open_LogIn_page_Click(object sender, RoutedEventArgs e)
@@ -37,6 +95,26 @@ namespace TeamUp
             Login login = new Login();
             login.Show();
             Close();
+        }
+
+
+        public int CheckEmail()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            int y = 0;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("email");
+                    if (attr != null) if (attr.Value == Text_Email.Text) y++;
+                }
+                if (y == 1) break;
+            }
+            return y;
         }
     }
 }
