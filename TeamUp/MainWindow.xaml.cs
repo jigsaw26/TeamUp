1af095ac19799f7a5cf11dba345fece27f3b2835
axxvxvx <80685257+jigsaw26@users.noreply.github.com>
2022-11-14 11:56:12 +0200

    rewq

--- TeamUp/MainWindow.xaml.cs
+++ TeamUp/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace TeamUp
 {
--- TeamUp/Register.xaml.cs
+++ TeamUp/Register.xaml.cs
@@ -23,17 +23,17 @@ namespace TeamUp
     /// </summary>
     public partial class Register : Window
     {
-        IFirebaseConfig Config = new FirebaseConfig { AuthSecret = "OwoqQp9UnoO3kygLu7OJL7mFXOdNL1oPHIbHyFLz", BasePath = "https://teamup-c8aff-default-rtdb.europe-west1.firebasedatabase.app" };
-        IFirebaseClient Client;
+     /*   IFirebaseConfig Config = new FirebaseConfig { AuthSecret = "OwoqQp9UnoO3kygLu7OJL7mFXOdNL1oPHIbHyFLz", BasePath = "https://teamup-c8aff-default-rtdb.europe-west1.firebasedatabase.app" };
+        IFirebaseClient Client;*/
 
         public Register()
         {
             InitializeComponent();
             this.WindowState = System.Windows.WindowState.Maximized;
-            Client = new FireSharp.FirebaseClient(Config);
+            //Client = new FireSharp.FirebaseClient(Config);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /*private void Button_Click(object sender, RoutedEventArgs e)
         {
             CreateUser();
             
@@ -47,10 +47,10 @@ namespace TeamUp
                 passwordV = Text_Counrty 
             };
             FirebaseResponse respone = await Client.SetAsync("users/"+Text_Name.Text, Info);
-            MessageBox.Show("Готово");
-        }
-
-       /* private void Button_Click(object sender, RoutedEventArgs e)
+            MessageBox.Show("Готово");  
+        }*/
+      
+        private void Button_Click(object sender, RoutedEventArgs e)
         {  
             if (Text_Name.Text == "" || Text_Surname.Text == "" || Text_Counrty.Text == "" || Text_Date.Text == "" || Text_Email.Text == "" || Text_Password.Password == "" || Text_Password2.Password == "")
                 MessageBox.Show("Заполните все поля");
@@ -58,8 +58,19 @@ namespace TeamUp
                 MessageBox.Show("Пароль не совпадает"); 
             else
             {
-                int Check = CheckEmail();
-                if (Check == 1) MessageBox.Show("Такой пользователь уже существует");
+                int Email_Check1 = CheckEmail();
+                if (Email_Check1 == 1) MessageBox.Show("Такой пользователь уже существует");
+                  
+                if (IsEmail(Text_Email.Text) == 0)
+                {
+                    MessageBox.Show("Вы ввели не правильный адресс электронной почты");
+                }
+
+                if (CheckPassword(Text_Password.Password) != 3)
+                {
+                    MessageBox.Show("Пароль не соответствует требованиям");
+                }
+
                 else
                 { 
                     string[] AtribteName = new string[6] { "User", "Name", "Surname", "Country", "Date", "Password" }; 
@@ -115,7 +126,7 @@ namespace TeamUp
         }
 
 
-        */
+
         private void Open_LogIn_page_Click(object sender, RoutedEventArgs e)
         {
             Login login = new Login();
@@ -124,23 +135,68 @@ namespace TeamUp
         }
 
 
-      /*  public int CheckEmail()
-        {
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.Load("Users.xml");
-            XmlElement xRoot = xDoc.DocumentElement;
 
-            int y = 0;
-            foreach (XmlNode xnode in xRoot)
+        public int CheckPassword(string value)
+        {
+            int x = 0;
+            if (value.Length >= 8)
             {
-                if (xnode.Attributes.Count > 0)
-                {
-                    XmlNode attr = xnode.Attributes.GetNamedItem("email");
-                    if (attr != null) if (attr.Value == Text_Email.Text) y++;
-                }
-                if (y == 1) break;
+                x++;
             }
-            return y;
-        }*/
+            var Symbols = new[] { '(', '_', '!', '"', '№', ';', '%', ':', '?', '*', ')', '"' };
+            if (value.Any(ch => Symbols.Contains(ch)))
+            {
+                x++;
+            }
+
+            int temp = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] >= '0' && value[i] <= '9')
+                {
+                    temp++;
+                }
+            }
+
+           
+            if (temp >= 1) x++;
+ 
+            return x;
+
+        }
+
+        public int IsEmail(string value)
+        {
+            int x = 0;
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(value);
+                x++;
+                return x;
+            }
+            catch
+            {
+                return x;
+            }
+        }
+
+        public int CheckEmail()
+          {
+              XmlDocument xDoc = new XmlDocument();
+              xDoc.Load("Users.xml");
+              XmlElement xRoot = xDoc.DocumentElement;
+
+              int y = 0;
+              foreach (XmlNode xnode in xRoot)
+              {
+                  if (xnode.Attributes.Count > 0)
+                  {
+                      XmlNode attr = xnode.Attributes.GetNamedItem("email");
+                      if (attr != null) if (attr.Value == Text_Email.Text) y++;
+                  }
+                  if (y == 1) break;
+              }
+              return y;
+          }
     }
 }
