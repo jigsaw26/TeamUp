234c6df3f89cc629d9bacfb4e6ab4543f09a7fc0
axxvxvx <80685257+jigsaw26@users.noreply.github.com>
2022-12-12 14:48:48 +0200

    Добавил страницу ивентов

---
+++ TeamUp/Events.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamUp
+{
+    /// <summary>
+    /// Логика взаимодействия для Events.xaml
+    /// </summary>
+    public partial class Events : Window
+    {
+        public Events()
+        {
+            InitializeComponent();
+        }
+
+        private void B_Home_Click(object sender, RoutedEventArgs e)
+        {
+            Home home = new Home();
+            CWindowState();
+            home.Show();
+            Close();
+        }
+
+        private void B_Settings_Click(object sender, RoutedEventArgs e)
+        {
+            if (С_Settigs.GetWindow() == 0)
+            {
+                С_Settigs.SetWindow(1);
+
+                Settings settings = new Settings();
+                settings.Owner = this;
+
+                if (settings.ShowDialog() == true) { }
+            }
+        }
+
+        private void B_Messages_Click(object sender, RoutedEventArgs e)
+        {
+            Messages messages = new Messages();
+            CWindowState();
+            messages.Show();
+            Close();
+        }
+
+        private void B_Notification_Click(object sender, RoutedEventArgs e)
+        {
+            InDev indev = new InDev();
+            CWindowState();
+            indev.Show();
+            Close();
+        }
+
+        private void B_Events_Click(object sender, RoutedEventArgs e)
+        {
+            //InDev indev = new InDev();
+            //CWindowState();
+            //indev.Show();
+            //Close();
+        }
+
+        private void B_Work_Click(object sender, RoutedEventArgs e)
+        {
+            InDev indev = new InDev();
+            CWindowState();
+            indev.Show();
+            Close();
+        }
+        void CWindowState()
+        {
+            C_WindowState.SetWindowStateHeight(this.Height);
+            C_WindowState.SetWindowStateWidth(this.Width);
+            C_WindowState.SetWindowStateTop(this.Top);
+            C_WindowState.SetWindowStateLeft(this.Left);
+        }
+    }
+}
